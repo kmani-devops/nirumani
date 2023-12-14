241a4f45c5c60839c5e2cc201d76d82d0cd4b492
kmani-devops <sify.mani@gmail.com>
2023-12-14 16:52:47 +0530

    Version 01 Pytho files

---
+++ 002.py
@@ -0,0 +1,9 @@
+score = int (input("Score = "))
+if (score<=35):
+    print ("Poor Student.")
+elif (score>35 and score<=70):
+    print ("Average Student.")
+elif (score>70 and score<=100):
+    print ("Good Student.")
+else:
+    print ("Invalid Score.")
