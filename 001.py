9235af5b1deac386a02bcd0f07222356ad430156
kmani-devops <sify.mani@gmail.com>
2024-02-29 22:37:52 +0530

    files modified

---
+++ 001.py
@@ -0,0 +1,14 @@
+a = int (input("A = ")):::::
+a = int (input("A = ")abc)
+if (a%2==0):
+    print ("Even No.")
+else:
+    print ("Odd No.")
+
+
+b = int (input("B = "))
+
+if (b%5==0):
+    print ("Divisible by 5.")
+else:
+    print ("Not Divisible.")
